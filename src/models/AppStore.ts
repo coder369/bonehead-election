a1ac06f6c6124449824bd5e8ee2b41d764d2917c
Yao Wang <yao_wang@intuit.com>
2020-11-12 11:56:26 -0800

    reducer for voter's actions

--- src/models/AppStore.ts
+++ src/models/AppStore.ts
@@ -1,9 +1,18 @@
 import { Election } from "./Election";
 import { Voter } from "./Voter";
 
+export type SortDir = 'asc' | 'desc';
+
+export type VotersSort = {
+    sortCol: keyof Voter;
+    sortDir: SortDir;
+}
+
 export type AppState = {
     elections: Election[],
-    voters: Voter[]
+    voters: Voter[],
     selectedElectionId: number,
     selectedVoterId: number,
+    editVoterId: number,
+    votersSort: VotersSort,
 };
--- src/reducers/appReducers.ts
+++ src/reducers/appReducers.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { electionsReducer, selectedElectionReducer } from "./electionsReducer";
-import { selectedVoterReducer, votersReducer } from "./votersReducer";
+import { selectedVoterReducer, votersReducer, editVoterIdReducer, votersSortReducer } from "./votersReducer";
 
 export const AppReducer = combineReducers({
     elections: electionsReducer,
     voters: votersReducer,
+    editVoterId: editVoterIdReducer,
+    votersSort: votersSortReducer,
     selectedElectionId: selectedElectionReducer,
     selectedVoterId: selectedVoterReducer,
 })
